5a90339a9f073fc8bd17eae9b9bbe48f509f3eb3
raresportan <rares.portan@3pillarglobal.com>
2021-07-04 08:53:41 +0300

    React playground implementation

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # react-playground
-A simple React playground
+A simple React playground.
+
+The playground is usable without any builds (just open `index.html` in a browser), but if you want to make changes to the editor:
+* Run `npm i` to install CodeMirror and Rollup libs
+* Run `npm start` to build your changes 
+
---
+++ editor.js
@@ -0,0 +1,77 @@
+import {EditorState, EditorView, basicSetup} from "@codemirror/basic-setup"
+import {javascript} from "@codemirror/lang-javascript"
+
+const jscode=`
+// Note:
+// 1. Use React.something instead of importing something
+// 2. Container id is 'app'
+
+
+function Counter({initialCount}) {
+  const [count, setCount] = React.useState(initialCount);
+  return (
+    <>
+      Count: {count}
+      <button onClick={() => setCount(initialCount)}>Reset</button>
+      <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+    </>
+  );
+}
+
+ReactDOM.render(
+  <Counter initialCount={1} />,
+  document.getElementById('app')
+);
+`;
+const myTheme = EditorView.baseTheme({
+  "&.cm-editor": {
+    fontSize: '16px',
+  },
+  ".cm-scroller": {
+    fontFamily:'Consolas, Menlo, Monaco, source-code-pro, Courier New, monospace'
+  },
+})
+
+let timer;
+
+const evaluateCode = (code) => {
+  console.log('Eval');
+  // console.clear();
+  try{
+    const sandbox = document.getElementById('sandbox');
+    sandbox.contentWindow.postMessage({ code }, '*');
+  }
+  catch(err) {
+    console.error(err);
+  }
+}
+
+let index = 0;
+
+const editor = new EditorView({
+  state: EditorState.create({
+    extensions: [
+      basicSetup, 
+      javascript(),
+      myTheme,
+      EditorView.updateListener.of((v)=> {
+        if(v.docChanged) {
+          if(timer) clearTimeout(timer);
+          timer = setTimeout(() => {
+            evaluateCode(editor.state.doc.toString())
+          }, 500 );
+        }
+      })
+    ],    
+    doc: jscode
+  }),
+  parent: document.getElementById('editor')
+})
+
+// first time evaluation
+window.addEventListener('DOMContentLoaded', (event) => {
+  setTimeout( () => {
+   evaluateCode(editor.state.doc.toString())
+  }, 1000);
+})
